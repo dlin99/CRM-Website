4d4fe9a8ba53ff66f684bbd864c5c85684c02edd
Danny Lin <linycdanny@gmail.com>
2020-11-29 18:34:53 +0800

    add password reset functionality

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # CRM-Website 
 CRM website built with django
 
+
 # Functions:
 
 # Tech & Tools: 
--- crm/settings.py
+++ crm/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
+    'accounts.apps.AccountsConfig',
 
     'django_filters',
 ]
@@ -131,3 +131,14 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = 'True' 
+EMAIL_HOST_USER = 'my_gmail_account'
+EMAIL_HOST_PASSWORD = 'my_gmail_password' # change to *** before uploading
