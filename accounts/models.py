c715518887613bee351e310d10876076761a11d7
Danny Lin <linycdanny@gmail.com>
2020-11-29 13:56:01 +0800

    set profile pics

--- accounts/models.py
+++ accounts/models.py
@@ -3,21 +3,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Customer(models.Model):
-	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
 	name = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
+	profile_pic = models.ImageField(default="profile1.png", null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-
 	def __str__(self):
-		return self.name
+		return str(self.name)
 
 class Tag(models.Model):
 	name = models.CharField(max_length=200, null=True)
 
 	def __str__(self):
-		return self.name
+		return str(self.name)
 
 
 class Product(models.Model):
@@ -33,7 +33,7 @@ class Product(models.Model):
 	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
-		return self.name
+		return str(self.name)
 
 class Order(models.Model):
 	STATUS = (
@@ -49,4 +49,4 @@ class Order(models.Model):
 	note = models.CharField(max_length=1000, null=True)
 
 	def __str__(self):
-		return self.product.name
+		return str(self.product.name)
